ad3bec4ddac92e191695510f3e622a5cb6a0a43d
jngourdol <jngourdol@stormancer.com>
2023-05-11 16:54:45 +0200

    Add project files.

---
+++ WizardsandWarriors/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using WizardsandWarriors.Models;
+
+var party = new List<Player>();
+
+var player1 = new Warrior();
+var player2 = new Wizard();
+
+player1.EquippedWeapon = new Staff();
+player2.EquippedWeapon = new Sword();
+
+party.Add(player1);
+party.Add(player2);
