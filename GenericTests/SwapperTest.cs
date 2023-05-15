506e527bee571213d6ea42fe99957168a30a0554
jngourdol <jngourdol@stormancer.com>
2023-05-15 10:47:55 +0200

    more unit tests for swapper

--- GenericTests/SwapperTest.cs
+++ GenericTests/SwapperTest.cs
@@ -1,4 +1,5 @@
 using GenericExercices;
+using System.Collections.Generic;
 
 namespace GenericTests
 {
@@ -6,7 +7,7 @@ namespace GenericTests
     public class SwapperTest
     {
         [TestMethod]
-        public void SwappReferencesInt()
+        public void SwapReferenceInt()
         {
             var i = 42;
             var j = 666;
@@ -18,7 +19,7 @@ namespace GenericTests
         }
 
         [TestMethod]
-        public void SwappReferencesString()
+        public void SwapReferenceString()
         {
             var i = "blah";
             var j = "bloh";
@@ -27,6 +28,42 @@ namespace GenericTests
 
             Assert.AreEqual(i, "bloh");
             Assert.AreEqual(j, "blah");
+        }
+
+        [TestMethod]
+        public void SwapArrayInt()
+        {
+            var array = new[] { 0, 1, 2, 3, 4, 5 };
+            Swapper.Swap(array, 2, 4);
+            Assert.IsTrue(array.Length == 6);
+            Assert.IsTrue(array.SequenceEqual(new[] { 0, 1, 4, 3, 2, 5 }));
+        }
+
+        [TestMethod]
+        public void SwapArrayString()
+        {
+            var array = new[] { "0", "1", "2", "3", "4", "5" };
+            Swapper.Swap(array, 2, 4);
+            Assert.IsTrue(array.Length == 6);
+            Assert.IsTrue(array.SequenceEqual(new[] { "0", "1", "4", "3", "2", "5" }));
+        }
+
+        [TestMethod]
+        public void SwapListInt()
+        {
+            var list = new List<int> { 0, 1, 2, 3, 4, 5 };
+            Swapper.Swap(list, 2, 4);
+            Assert.IsTrue(list.Count == 6);
+            Assert.IsTrue(list.SequenceEqual(new[] { 0, 1, 4, 3, 2, 5 }));
+        }
+
+        [TestMethod]
+        public void SwapListString()
+        {
+            var list = new SynchronizedCollection<string> { "0", "1", "2", "3", "4", "5" };
+            Swapper.Swap(list, 2, 4);
+            Assert.IsTrue(list.Count == 6);
+            Assert.IsTrue(list.SequenceEqual(new[] { "0", "1", "4", "3", "2", "5" }));
         }
     }
 }
